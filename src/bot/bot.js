47491e1149aab4c3a572ffa54fa1f14e443b99eb
Jackson202-svg <zwtwwkg65k@privaterelay.appleid.com>
2026-02-10 13:17:33 +0900

    Update bot.js

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,13 +1,16 @@
 import { responses } from "./responses.js";
+import { intents } from "./intents.js";
+import { remember } from "./memory.js";
 
 export function getBotResponse(text) {
-  const message = text.toLowerCase();
+  const msg = text.toLowerCase();
+  remember(msg);
 
-  for (const key in responses) {
-    if (message.includes(key)) {
-      return responses[key];
+  for (const intent in intents) {
+    if (intents[intent].some(word => msg.includes(word))) {
+      return responses[intent]();
     }
   }
 
-  return "🤖 I'm still learning. Try typing 'help'.";
+  return "🤖 I'm still learning. Try 'help'.";
 }
