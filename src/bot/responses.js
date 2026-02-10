93c9354eb9c3d0f9c751b6dbcdca1337677b74d7
Jackson202-svg <zwtwwkg65k@privaterelay.appleid.com>
2026-02-10 13:09:20 +0900

    Create responses.js

---
+++ src/bot/responses.js
@@ -0,0 +1,7 @@
+export const responses = {
+  "hello": "Hello! 👋 How can I help you?",
+  "help": "Try asking about time, date, or say hello!",
+  "time": "🕒 " + new Date().toLocaleTimeString(),
+  "date": "📅 " + new Date().toLocaleDateString(),
+  "bye": "Goodbye! 👋 Come back soon."
+};
